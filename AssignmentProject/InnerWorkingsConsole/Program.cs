074a0789c1ae0543dbe2969185ad34a478d731b2
Vaclav Pfeifer <vaclav.pfeifer@gmail.com>
2015-10-21 00:33:27 +0200

    missing files.

---
+++ AssignmentProject/InnerWorkingsConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerWorkingsConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load input data - either from file or by parsing input arguments
+            // Results to console/to file?
+
+
+            // Init all mappings (unity + automapper)
+
+
+            // Process input Jobs
+
+
+
+
+        }
+    }
+}
---
+++ AssignmentProject/InnerWorkingsTests/Processors/ItemProcessorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InnerWorkings.Processors.Tests
+{
+    [TestClass()]
+    public class ItemProcessorTests
+    {
+        [TestMethod()]
+        public void CalculatePriceTest_ShouldOmmitTaxWhenTaxFree()
+        {
+            var item = new Item(true)
+            {
+                Name = "Letter",
+                Price = (float) 100.0
+            };
+
+            Assert.AreEqual(111, new ItemProcessor().CalculatePrice(item, 11, 5));
+        }
+
+        [TestMethod()]
+        public void CalculatePriceTest_ShouldIncludeTaxAtDefault()
+        {
+            var item = new Item
+            {
+                Name = "Letter",
+                Price = (float) 100.0
+            };
+
+            Assert.AreEqual(116, new ItemProcessor().CalculatePrice(item, 11, 5));
+        }
+    }
+}

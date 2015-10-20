5474a71da0512e4b4ab0a695b592ba54da39dce3
Vaclav Pfeifer <vaclav.pfeifer@gmail.com>
2015-10-20 23:35:38 +0200

    Basic structure done

---
+++ AssignmentProject/InnerWorkings/Item.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerWorkings
+{
+    public class Item
+    {
+        private readonly bool _isTaxFree;
+
+        public Item(bool isTaxFree = false)
+        {
+            this._isTaxFree = isTaxFree;
+        }
+
+        public bool IsTaxFree
+        {
+            get
+            {
+                return this._isTaxFree;
+            }
+        }
+
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+
+    public class LetterHead : Item
+    {
+        public LetterHead(bool isTaxFree = false) 
+            : base(isTaxFree)
+        {
+        }
+    }
+
+    public class Envelope : Item
+    {
+        public Envelope(bool isTaxFree = false)
+            : base(isTaxFree)
+        {
+        }
+    }
+
+    public class BusinessCard : Item
+    {
+        public BusinessCard(bool isTaxFree = false)
+            : base(isTaxFree)
+        {
+        }
+    }
+
+}
--- AssignmentProject/InnerWorkings/Job.cs
+++ AssignmentProject/InnerWorkings/Job.cs
@@ -8,5 +8,52 @@ namespace InnerWorkings
 {
     public class Job
     {
+        public List<Item> Items { get; set; }
+
+        public IMargin Margin { get; set; }
+
+        public ITax Tax { get; set; }
     }
+
+    public interface IJobProcessor
+    {
+        JobResultSet ProcessJob(Job job, IItemPriceCalculator itemPriceCalculator);
+    }
+
+    public class JobProcessor : IJobProcessor
+    {
+        JobResultSet IJobProcessor.ProcessJob(Job job, IItemPriceCalculator itemPriceCalculator)
+        {
+            var jobResult = new JobResultSet();
+
+            foreach(var item in job.Items)
+            {
+                float itemPrice = itemPriceCalculator.CalculatePrice(
+                    item, job.Margin.GetMargin(), job.Tax.getTax());
+
+                jobResult.Results.Add(new Tuple<string, float>(item.Name, itemPrice));
+            }
+
+            return jobResult;
+        }
+    }
+
+    public class JobResultSet
+    {
+        public JobResultSet()
+        {
+            this.Results = new List<Tuple<string, float>>();
+        }
+
+        public List<Tuple<string, float>> Results { get; private set; }
+    }
+
+
+    //public class JobProcessorParallel : IJobProcessor
+    //{
+    //    void IJobProcessor.CalculateJob(Job job)
+    //    {
+    //        Parallel.ForEach<Item>(job.Items, )
+    //    }
+    //}
 }
